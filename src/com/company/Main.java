3b22715dab132ff253a6e8f82f8b364444b59ffa
bigelbrus <bigelbrus@gmail.com>
2018-01-29 23:33:35 +0300

    Ввод аргументов списком

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Animal[] animals = {new Cat("Barsik",2,40),new Dog("Sharik", 2,150,100), new Cat("Cat", 3,1)};
+//        Animal[] animals = {new Cat("Barsik",2,40),new Dog("Sharik", 2,150,100), new Cat("Cat", 3,1)};
 
-        Obstacle[] obstacles = {new Wall(1), new Lake(50)};
+//        Obstacle[] obstacles = {new Wall(1), new Lake(50)};
 
-        Team team = new Team("First",animals);
-        Marathon marathon = new Marathon("Main", obstacles);
+        Team team = new Team("First",new Challengeable[]{new Cat("Barsik",2,40),new Dog("Sharik", 2,150,100), new Cat("Cat", 3,1)});
+        Marathon marathon = new Marathon("Main", new Obstacle[]{new Wall(1), new Lake(50)});
         marathon.Game(team);
         team.info();
         }
